d9d48b710aaa67b9ce13c8bcbe867f073114f9c7
rcjhd <52388572+rcjhd@users.noreply.github.com>
2019-07-10 15:54:49 +0800

    拓展mongodb类，加入存取背景音乐的函数

--- CODE/back/java/com/cpd/soundbook/MongoDB/MongoDBImpl.java
+++ CODE/back/java/com/cpd/soundbook/MongoDB/MongoDBImpl.java
@@ -9,6 +9,7 @@ import com.mongodb.gridfs.GridFSInputFile;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
+import java.util.List;
 
 @Repository
 public class MongoDBImpl implements MongoDBInter{
@@ -51,5 +52,33 @@ public class MongoDBImpl implements MongoDBInter{
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void saveBackgroundMusic(File file,int level){
+        try {
+            GridFS gridFS = new GridFS(client.getDB("audiobook"));
+            GridFSInputFile gridFSInputFile = gridFS.createFile(file);
+            gridFSInputFile.put("filename",file.getName());
+            gridFSInputFile.put("level",level);
+            gridFSInputFile.put("contentType", "application/mp3");
+            gridFSInputFile.save();
+            file.delete();//删除临时文件
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<GridFSDBFile> getBackgroundMusic(int level){
+        List<GridFSDBFile> list=null;
+        try{
+            GridFS gridFS = new GridFS(client.getDB("audiobook"));
+            DBObject dbObject = new BasicDBObject("level",level);
+            list= gridFS.find(dbObject);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
     }
 }
